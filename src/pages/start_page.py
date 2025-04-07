51b4eecd23c268a0cd3d8b2662d690f0ca0278f2
David Andersson <lejonmanen@gmail.com>
2025-04-07 16:01:16 +0200

    Finished steps for add_players feature

---
+++ src/pages/start_page.py
@@ -0,0 +1,1 @@
+# TODO: här ska gemensamma funktioner läggas
--- src/steps/step_add_players.py
+++ src/steps/step_add_players.py
@@ -1,6 +1,16 @@
 import re
 
+from playwright.sync_api import expect
 from behave import given, when, then
+
+
+# TODO: flytta till page-fil
+def fill_player_name(page, name):
+    page.get_by_role("textbox").fill(name)
+
+def player_visible(page, name):
+    return page.locator(".player").get_by_text(re.compile(name + "\s+0:00.0"))
+
 
 @given(u'spelaren är på startsidan')
 def step_given_start_page(context):
@@ -12,25 +22,22 @@ def step_when_add_player(context):
     add_button = context.page.get_by_role("button").get_by_text("Lägg till spelare")
     add_button.click(timeout=100)
 
-def fill_player_name(page, name):
-    page.get_by_role("textbox").fill(name)
 
 @when(u'spelaren skriver "David" i textfältet')
-def step_impl(context):
-    #context.page.get_by_role("textbox").fill("David")
+def step_when_player_types_name(context):
     fill_player_name(context.page, "David")
 
-
-@then(u'"David" dyker upp på sidan med texten "0:00.0"')
-def step_impl(context):
-    context.page.get_by_text(re.compile("David\s+0:00.0"))
-
-
 @when(u'spelaren skriver "Gerson" i textfältet')
-def step_impl(context):
+def step_when_player_types_name_2(context):
     fill_player_name(context.page, "Gerson")
 
 
+@then(u'"David" dyker upp på sidan med texten "0:00.0"')
+def step_player_name_visible(context):
+    element = player_visible(context.page, "David")
+    expect(element).to_be_visible()
+
 @then(u'"Gerson" dyker upp på sidan med texten "0:00.0"')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then "Gerson" dyker upp på sidan med texten "0:00.0"')
+def step_then_player_name_visible_2(context):
+    element = player_visible(context.page, "Gerson")
+    expect(element).to_be_visible()
